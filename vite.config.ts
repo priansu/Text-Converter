bd161cd02bfb438ed1868aef6a453b61d225bf94
Priansu Kumar Rai <priansurai@gmail.com>
2026-05-22 19:43:32 +0530

    added site map plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemapPlugin from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemapPlugin({
+      hostname: 'http://localhost:3000/',
+      generateRobotsTxt: true,
+      exclude: ['/404']
+    })
+  ],
   server: {
     port: 3000,
     open: true
